4c9149de08257b0423a1f56d00c4bd1d7ea8b522
Hunter Clezie <clezhunt@gmail.com>
2026-04-19 20:38:46 -0600

    add draw sine wave method

--- EtchOSketch/EtchOSketch.cs
+++ EtchOSketch/EtchOSketch.cs
@@ -38,6 +38,33 @@ namespace EtchOSketch
             thePen.Dispose();
         }
 
+        void DrawSineWave()
+        {
+            //create a graphics object named g that draws on the picture box
+            Graphics g = DisplayPictureBox.CreateGraphics();
+            //create a pen to draw with
+            Pen thePen = new Pen(Color.Lime);
+            int lastX = 0, lastY = 0, currentY = 0;
+            float scaleX = DisplayPictureBox.Width / 360f;
+            float scaleY = (DisplayPictureBox.Height / 200f) * -1;
+
+            g.TranslateTransform(0, DisplayPictureBox.Height / 2);
+
+            g.ScaleTransform(scaleX, scaleY);
+
+            for (int currentX = 0; currentX < 360; currentX++)
+            {
+                currentY = (int)(Math.Round(100 * Math.Sin((Math.PI / 180) * currentX)));
+                g.DrawLine(thePen, lastX, lastY, currentX, currentY);
+                lastX = currentX;
+                lastY = currentY;
+            }
+
+            //free up resorces
+            g.Dispose();
+            thePen.Dispose();
+        }
+
         //Event Handlers below here --------------------------------------------------------------
 
         private void ExitButton_Click(object sender, EventArgs e)
